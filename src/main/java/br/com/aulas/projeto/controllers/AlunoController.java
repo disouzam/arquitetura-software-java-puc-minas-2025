0c7aafe7e4c761f8f523afa0e6ed2c204c474519
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-06-10 22:07:50 -0300

    Modified Get All Alunos endpoint to return a fixed list of alunos

--- src/main/java/br/com/aulas/projeto/controllers/AlunoController.java
+++ src/main/java/br/com/aulas/projeto/controllers/AlunoController.java
@@ -1,5 +1,6 @@
 package br.com.aulas.projeto.controllers;
 
+import br.com.aulas.projeto.entities.Aluno;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +8,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "alunos")
 public class AlunoController {
 
     @GetMapping
-    public ResponseEntity<?> recuperaListaAlunos() {
-        return new ResponseEntity<>("Get Alunos", HttpStatus.OK);
+    public ResponseEntity<List<Aluno>> getAlunos() {
+        List<Aluno> alunos = new ArrayList<Aluno>();
+        alunos.add(new Aluno(1, "Aluno 1"));
+        alunos.add(new Aluno(2, "Aluno 2"));
+        alunos.add(new Aluno(3, "Aluno 3"));
+
+        return new ResponseEntity<List<Aluno>>(alunos, HttpStatus.OK);
     }
 
     @PostMapping
